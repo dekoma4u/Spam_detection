8bb4adab26f9441b7fdd4007c0d28b8ab1edd5c1
Ugoo Ezekoma <ugoo.f.ezekoma@gmail.com>
2022-11-06 22:52:37 +0100

    Update added

--- spam_detector.py
+++ spam_detector.py
@@ -19,20 +19,16 @@ x = df["EmailText"]
 y = df["Label"]
 
 # Split the test to avoid either overfitting or underfitting.
-
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 # Instantiate the CountVectorizer for tokenization and fit_transform the test data
-
 cv = CountVectorizer()
 features = cv.fit_transform(x_train)
 
 # Instantiate the Model and fit the tokenized train dataset and the y_train (target value)
-
 model = svm.SVC()
 model.fit(features, y_train)
 
 # Use the test datasets to test the model for accuracy score. Recall to transform the x_test to tokens
-
 features_test = cv.transform(x_test)
 print("Accuracy score: {}".format(model.score(features_test, y_test)))
